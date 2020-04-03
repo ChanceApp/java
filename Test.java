941e14c67406f9ed9bbfbeac13724a84ad9e7c25
ChanceApp <49782599+ChanceApp@users.noreply.github.com>
2020-04-03 11:18:51 +0800

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,10 @@
+//学习Java第一天
+//给世界大哥招呼
+//定义一个Test类
+public class Test{
+//启动main方法，没有这个执行不了程序
+public static void main(String[]args){
+//系统输出打印字符串：HELLO WORLD
+      System.out.println("HELLO WORLD");//java语句要以分号(;)结尾,否则程序会报错.
+      }
+   }
